19f2f096b8d3279f4bdc5c9d44678e413942582d
Javier <javiergrana@hotmail.com>
2020-09-16 19:44:22 -0300

    Agregados de la clase 2

---
+++ Clase 1/ErroresYLog/ErroresYLog/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ErroresYLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 0;
+
+            int resultado;
+
+
+            try
+            {
+                DivisionSegura(10, 0);
+            }            
+            catch (DivideByZeroException ex2)
+            {
+                Console.WriteLine("Se produjo un error de de división por cero");
+                //throw;
+            }
+            catch (Exception ex1)
+            {
+                Console.WriteLine("Se produjo un error desconocido");
+                Console.WriteLine(ex1.ToString());
+            }
+
+        }
+
+        public static int DivisionSegura(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();                
+            }
+            return a / b;
+
+        }
+    }
+}
---
+++ Clase 2/Composicion/Composicion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Composicion
+{
+    public class Motor
+    {
+        public Motor()
+        {
+            
+        }
+    }
+
+    public class Rueda
+    {
+        public Rueda()
+        {
+            
+        }
+    }
+
+    public class Vehiculo
+    {
+        private Motor MotorDeVehiculo;
+        public Rueda Ruedas { get; set; }
+        public Vehiculo(Rueda Ruedas)
+        {
+            this.Ruedas = Ruedas; // Agregación 
+            MotorDeVehiculo = new Motor();// Composición 
+        }      
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rueda Fate = new Rueda();
+            Vehiculo Ford = new Vehiculo(Fate);         
+        }
+    }
+}
